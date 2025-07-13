08b361f37ecd7669b121c2ed5ae7f59bd801771c
developerjhp <developerjhp@gmail.com>
2025-07-14 01:35:24 +0900

    feat: 독서 상태 관련 상수 및 타입 추가
    - 독서 상태 값을 정의하는 ReadingStatus 타입을 추가하였습니다.
    - 각 독서 상태에 대한 레이블을 정의하는 READING_STATUS_LABELS 상수를 추가하였습니다.

--- src/constants/book.ts
+++ src/constants/book.ts
@@ -12,12 +12,21 @@ export const READING_STATUS_VALUES = [
   READING_STATUS.PAUSE,
 ] as const;
 
+export type ReadingStatus = (typeof READING_STATUS_VALUES)[number];
+
 export const READING_STATUS_OPTIONS = [
   { value: READING_STATUS.WISH, label: '읽고 싶은 책' },
   { value: READING_STATUS.READING, label: '읽는 중' },
   { value: READING_STATUS.DONE, label: '읽음' },
   { value: READING_STATUS.PAUSE, label: '보류 중' },
 ];
+
+export const READING_STATUS_LABELS = {
+  [READING_STATUS.WISH]: '읽고 싶은 책',
+  [READING_STATUS.READING]: '읽는 중',
+  [READING_STATUS.DONE]: '읽음',
+  [READING_STATUS.PAUSE]: '보류 중',
+} as const;
 
 export const BOOK_FORM_STEPS = [
   '도서 정보',
