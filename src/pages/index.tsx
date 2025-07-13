140bffe3c2948c5895e7f5d15d7985f72270dc5d
developerjhp <developerjhp@gmail.com>
2025-07-14 01:34:49 +0900

    feat:  AppPreview 추가
    - useWatch 훅을 사용하여 폼 데이터 실시간 감시 기능을 추가하였습니다.(for App Preview)
    - BOOK_FORM_DEFAULT_VALUES를 사용하여 기본값을 설정하였습니다.
    - AppPreview 컴포넌트를 추가하여 데스크탑 화면에서 도서 정보를 미리 볼 수 있도록 하였습니다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
-import { useForm, FormProvider } from 'react-hook-form';
+import { useForm, FormProvider, useWatch } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { bookFormSchema, type BookFormSchema } from '@/utils/schema';
+import {
+  bookFormSchema,
+  type BookFormSchema,
+  BOOK_FORM_DEFAULT_VALUES,
+} from '@/utils/schema';
 import BookInfoStep from '@/components/form/steps/BookInfoStep';
 import Stepper from '@/components/form/Stepper';
 import styled from '@emotion/styled';
@@ -9,24 +13,27 @@ import Card from '@/components/ui/Card';
 import { color } from '@/styles/colors';
 import { fontSize, fontWeight } from '@/styles/fonts';
 import { BOOK_FORM_STEPS } from '@/constants/book';
+import { AppPreview } from '@/components/preview/AppPreview';
+import {
+  DESKTOP_MIN_WIDTH,
+  useIsAboveMinWidth,
+} from '@/hooks/useIsAboveMinWidth';
 
 export default function HomePage() {
+  const isDesktop = useIsAboveMinWidth(DESKTOP_MIN_WIDTH);
+
   // TODO: PRD 3.4 - URL 쿼리 파라미터로 현재 단계 관리
   const currentStep = 1;
 
   const methods = useForm<BookFormSchema>({
     resolver: zodResolver(bookFormSchema),
     mode: 'all',
-    defaultValues: {
-      title: '',
-      author: '',
-      publishedDate: undefined,
-      totalPages: 0,
-      status: undefined,
-      startDate: undefined,
-      endDate: undefined,
-    },
+    defaultValues: BOOK_FORM_DEFAULT_VALUES,
   });
+
+  const formData = useWatch({
+    control: methods.control,
+  }) as BookFormSchema;
 
   const onSubmit = (data: BookFormSchema) => {
     // 최종 제출 로직
@@ -41,23 +48,35 @@ export default function HomePage() {
           <Description>단계별로 독서 경험을 기록해보세요</Description>
         </HeaderSection>
 
-        <Card>
-          <Stepper steps={BOOK_FORM_STEPS} currentStepIndex={currentStep - 1} />
-          <FormProvider {...methods}>
-            <StyledForm onSubmit={methods.handleSubmit(onSubmit)} noValidate>
-              {currentStep === 1 && <BookInfoStep />}
-              {/* TODO: 2~5단계 컴포넌트 추가 */}
-
-              <FormActionButtons
-                currentStep={currentStep}
-                onBack={() => {
-                  /* TODO: 이전 단계로 이동 */
-                }}
-                onReset={() => methods.reset()}
+        <PageLayout>
+          <FormWrapper>
+            <Card>
+              <Stepper
+                steps={BOOK_FORM_STEPS}
+                currentStepIndex={currentStep - 1}
               />
-            </StyledForm>
-          </FormProvider>
-        </Card>
+              <FormProvider {...methods}>
+                <StyledForm
+                  onSubmit={methods.handleSubmit(onSubmit)}
+                  noValidate
+                >
+                  {currentStep === 1 && <BookInfoStep />}
+                  {/* TODO: 2~5단계 컴포넌트 추가 */}
+
+                  <FormActionButtons
+                    currentStep={currentStep}
+                    onBack={() => {
+                      /* TODO: 이전 단계로 이동 */
+                    }}
+                    onReset={() => methods.reset()}
+                  />
+                </StyledForm>
+              </FormProvider>
+            </Card>
+          </FormWrapper>
+
+          {isDesktop && <AppPreview bookInfo={formData} />}
+        </PageLayout>
       </Container>
     </PageContainer>
   );
@@ -93,6 +112,20 @@ const Title = styled.h1`
 const Description = styled.p`
   color: ${color.gray600};
   font-size: ${fontSize.base};
+`;
+
+const PageLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 2rem;
+
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 400px;
+  }
+`;
+
+const FormWrapper = styled.div`
+  width: 100%;
 `;
 
 const StyledForm = styled.form`
